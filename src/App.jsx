572a1a922bce7c4011dd7f6666668a0049dfd347
karthik <karthik.cs1720@gmail.com>
2022-10-06 23:31:43 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-import Input from "./components/Input";
 import cs from "./cs.json";
+
 function App() {
   const [inputVal, setInputVal] = useState({
     name: "",
@@ -12,16 +12,6 @@ function App() {
     city: "",
     message: "",
   });
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setInputVal({
-      ...inputVal,
-      [name]: value,
-    });
-  };
-
-  // ==========================================
   const [validation, setValidation] = useState({
     name: "",
     email: "",
@@ -31,37 +21,56 @@ function App() {
     city: "",
     message: "",
   });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputVal((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-  const handleSubmit = async () => {
-    console.log("clicked");
-    const errors = validation;
-    const { name, mobile } = inputVal;
-    if (!name.trim()) {
-      errors.name = "Please enter the name";
+  // ==========================================
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = inputVal.name;
+    // name
+    if (!name) {
+      setValidation((prev) => ({ ...prev, name: "Enter the name" }));
     } else if (name.trim().length < 2) {
-      errors.name = "Enter full name";
+      setValidation((prev) => ({ ...prev, name: "Enter full name" }));
     } else if (name.trim().match(/[0-9]/)) {
-      errors.name = "Name cannot contain numbers";
-    } else errors.name = "";
-    //email
+      setValidation((prev) => ({
+        ...prev,
+        name: "Name cannot contain number",
+      }));
+    } else {
+      setValidation((prev) => ({ ...prev, name: "" }));
+    }
+    // email
     const emailCond =
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/;
     if (!inputVal.email.trim()) {
-      errors.email = "Email is required";
+      setValidation((prev) => ({ ...prev, email: "Email is required" }));
     } else if (!inputVal.email.match(emailCond)) {
-      errors.email = "Please enter a valid email address";
+      setValidation((prev) => ({ ...prev, email: "Enter valid email" }));
     } else {
-      errors.email = "";
+      setValidation((prev) => ({ ...prev, email: "" }));
     }
-    //password
+    //mobile
+    const mobile = inputVal.mobile;
     const phCond = /[0-9]/;
-    if (!mobile.trim()) {
-      errors.mobile = "Enter a mobile number";
-    } else if (mobile.trim().length < 10 || mobile.trim().length > 10) {
-      errors.mobile = "Enter a valid 10 digits mobile number";
-    } else if (!mobile.trim().match(phCond)) {
-      errors.mobile = "Mobile number should be in numbers";
-    } else errors.mobile = "";
+    if (mobile) {
+      setValidation((prev) => ({ ...prev, mobile: "Enter mobile number" }));
+    } else if (inputVal.mobile.length < 10 || inputVal.mobile.length > 10) {
+      setValidation((prev) => ({ ...prev, mobile: "Enter valid number" }));
+    } else if (!inputVal.mobile.trim().match(phCond)) {
+      setValidation((prev) => ({
+        ...prev,
+        mobile: "Ph number cannot contain letters",
+      }));
+    } else {
+      setValidation((prev) => ({ ...prev, mobile: "" }));
+    }
 
     for (
       let index = 0, countryFound = false;
@@ -73,7 +82,7 @@ function App() {
         cs.countries[index].country.toLowerCase()
       ) {
         countryFound = true;
-        errors.country = "";
+        setValidation((prev) => ({ ...prev, country: "" }));
         for (
           let indexb = 0, stateFound = false;
           indexb < cs.countries[index].states.length && stateFound === false;
@@ -83,64 +92,92 @@ function App() {
             cs.countries[index].states[indexb].toLowerCase() ===
             inputVal.state.trim().toLowerCase()
           ) {
-            errors.state = "";
+            setValidation((prev) => ({ ...prev, state: "" }));
             stateFound = true;
           } else {
-            errors.state = "State name invalid";
+            setValidation((prev) => ({ ...prev, state: "State name invalid" }));
           }
         }
       } else {
-        errors.country = "Country name invalid";
-        errors.state = "Please check country name";
+        setValidation((prev) => ({ ...prev, state: "State invalid" }));
+        setValidation((prev) => ({ ...prev, country: "Country invalid" }));
       }
     }
-    setValidation(errors);
   };
 
   return (
     <div className="FormContainer">
-      <div className="FormWrapper">
+      <form className="FormWrapper">
         <div className="FormTitleContainer">
           <h1 className="FormTitle">Registration</h1>
         </div>
         <div className="InputsContainer">
           <div className="Inputs">
-            <Input
-              name="name"
-              value={inputVal.name}
-              onChange={handleChange}
-            ></Input>
-            <span>{validation.name}</span>
-            <Input
-              name="email"
-              value={inputVal.email}
-              onChange={handleChange}
-              span={validation.email}
-            ></Input>
-            <Input
-              name="mobile"
-              value={inputVal.mobile}
-              onChange={handleChange}
-              span={validation.mobile}
-            ></Input>
-            <Input
-              name="country"
-              value={inputVal.country}
-              onChange={handleChange}
-              span={validation.country}
-            ></Input>
-            <Input
-              name="state"
-              value={inputVal.state}
-              onChange={handleChange}
-              span={validation.state}
-            ></Input>
-            <Input
-              name="city"
-              value={inputVal.city}
-              onChange={handleChange}
-              span={validation.city}
-            ></Input>
+            <div className="InputContainer">
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                className="InputArea"
+                value={inputVal.value}
+                onChange={handleChange}
+              />
+              {validation.name && <span>{validation.name}</span>}
+            </div>
+            <div className="InputContainer">
+              <label>Email</label>
+              <input
+                type="text"
+                name="email"
+                className="InputArea"
+                value={inputVal.value}
+                onChange={handleChange}
+              />
+              {validation.name && <span>{validation.email}</span>}
+            </div>
+            <div className="InputContainer">
+              <label>Mobile</label>
+              <input
+                type="text"
+                name="mobile"
+                className="InputArea"
+                value={inputVal.value}
+                onChange={handleChange}
+              />
+              {validation.name && <span>{validation.mobile}</span>}
+            </div>
+            <div className="InputContainer">
+              <label>Country</label>
+              <input
+                type="text"
+                name="country"
+                className="InputArea"
+                value={inputVal.value}
+                onChange={handleChange}
+              />
+              {validation.name && <span>{validation.country}</span>}
+            </div>
+            <div className="InputContainer">
+              <label>State</label>
+              <input
+                type="text"
+                name="state"
+                className="InputArea"
+                value={inputVal.value}
+                onChange={handleChange}
+              />
+              {validation.name && <span>{validation.state}</span>}
+            </div>
+            <div className="InputContainer">
+              <label>City</label>
+              <input
+                type="text"
+                name="city"
+                className="InputArea"
+                value={inputVal.value}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div className="TextBox">
             <textarea
@@ -148,14 +185,13 @@ function App() {
               value={inputVal.message}
               className="TextArea"
               onChange={handleChange}
-              span={validation.message}
             ></textarea>
           </div>
         </div>
-        <button className="FormSubmit" onClick={handleSubmit}>
+        <button type="submit" onClick={handleSubmit} className="FormSubmit">
           Submit
         </button>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -11,6 +11,7 @@ function Input(props) {
         value={props.value}
         onChange={props.onChange}
       />
+      <span>{props.span}</span>
     </div>
   );
 }
